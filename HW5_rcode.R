1fa7af48f66f020308dca8f48e9cc2ae8d1a56c4
Born2Ride <ccirelli2@gmail.com>
2018-09-30 11:47:00 -0400

    Continue work on P1

--- HW5_rcode.R
+++ HW5_rcode.R
@@ -19,38 +19,91 @@ fXY = c(((1+1)*(1/36)), ((1+2)*(1/36)), ((1+3)*(1/36)), ((2+1)*(1/36)),
         ((3+3)*(1/36)))
 sum_fXY = sum(fXY)     # equal 1
 
-# a.) c
+# a.) c  ----------------------------------------------------
 sum(fXY)
 
-# b.) P(X = 1 & Y < 4)
+# b.) P(X = 1 & Y < 4) --------------------------------------
 sum(fXY[x < 2 & y<4])
 
-# c.) P(X = 1)
+# c.) P(X = 1)------------------------------------------------
 sum(fXY[x<2])
 
-# d.) F(Y = 2)
+# d.) F(Y = 2)------------------------------------------------
 sum(fXY[y==2])
 
-# e.) P(X <_ 2, Y<_ 2)
+# e.) P(X <_ 2, Y<_ 2)----------------------------------------
 sum(fXY[x<2|x==2 & y<2|y==2])
 
-#f. E(X):  Expected value of X
-#          sum P(X=x) for each value of x in the range of x. 
+#f. E(X):  Expected value of X--------------------------------
+#          sum P(X=xi)*xi for each value of x in the range of x. 
 x_1 = fXY[x==1]
 x_2 = fXY[x==2]
 x_3 = fXY[x==3]
 E_x = sum(c((x_1*1) + (x_2*2) + (x_3*3)))
 E_x
 
-# g.) E(Y)
-
+# g.) E(Y)----------------------------------------------------
 y_1 = fXY[y==1]
 y_2 = fXY[y==2]
 y_3 = fXY[y==3]
 E_y = sum(c((y_1*1) + (y_2*2) + (y_3*3)))
 E_y
 
+# h.) Var(X)--------------------------------------------------
+#     sum (x-mu of x)^2
+Var_x = c((1 - E_x)^2 + (2 - E_x)^2 + (3 - E_x)^2)
+Var_x
 
+# i.) Var(Y)--------------------------------------------------
+Var_y = c((1- E_y)^2 + (2 - E_y)^2 + (2 - E_y)^2)
+Var_y
+
+# j.) Marginal Probability Distribution x ---------------------
+sum_x_1 = sum(x_1)
+sum_x_2 = sum(x_2)
+sum_x_3 = sum(x_3)
+
+# k.) Conditional Probability Distribution y given X = 1--------
+
+# P(y=1 & x=1)
+p_x1 = sum(fXY[x ==1])
+
+p_y1_x1 = fXY[y==1 & x==1]
+p_y2_x1 = fXY[y==2 & x==1]
+p_y3_x1 = fXY[y==3 & x==1]  
+
+p_y1_x1 / p_x1
+p_y2_x1 / p_x1
+p_y3_x1 / p_x1  
+
+
+# l.) Conditional Probability Distribution of X given Y = 2-----
+
+# P(y=2 & x=1)
+p_y2_x1 = fXY[y==2 & x==1]
+p_y2_x1
+# P(y=2 | x=1)
+p_y2_x1 / sum_x_1
+
+# P(y=3 & x=1)
+p_y3_x1 = fXY[y==3 & x==1]
+# P(y=3 | x=1)
+p_y3_x1
+p_y3_x1 / sum_x_1
+
+# l.) P(x | y=2)
+p_x2 = sum(fXY[x==2])
+p_x2
+
+p_x1_y2 = fXY[x==1 & y==2]
+p_x2_y2 = fXY[x==2 & y==2]
+p_x3_y2 = fXY[x==2 & y==2]
+
+p_x1_y2 / p_x2
+p_x2_y2 / p_x2
+p_x3_y2 / p_x2
+
+# m.) E(Y|X = 1)
 
 
 
