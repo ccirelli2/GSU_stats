5801585d6abf27c8c64b49ba5081fc6c60f36382
Born2Ride <ccirelli2@gmail.com>
2018-12-12 17:23:12 -0500

    Ch1 continued script

---
+++ R_Statistics_Book/Ch1_cont.R
@@ -0,0 +1,104 @@
+# Ch1_continued_________________________________________________________________________________
+
+rm(list = ls())
+
+# Objects---------------------------------------------------------------------------------------
+
+x<- 2                                       # book says x is a vector of length 1.  mode(x) says integer. 
+x <- vector()                               # book says a vector of length 0.  mode(x) returns logical. 
+x <- matrix()                               # book says x is a matrix of 1 column, 1 row. 
+x <- function(){}                           # book says that x is a function that does nothing
+
+
+# Departure (cool functions)
+x <- letters[1:4]                           # returns letters coinciding with their index value. 
+cbind(x)                                     # binds vectors as columns. 
+
+# Inspecting Objects
+x <- c(1,2,3,4)
+typeof(x)
+class(x)
+mode(x)
+dim(x)                                      # object dimension 
+dimnames()                                  # for dimension names
+attributes(x) 
+https://www.youtube.com/watch?v=H3M0E-BNplQ
+
+# Return Vector of Object Type & Coercion 
+
+x <- numeric(3)
+x <- integer(3)
+x <- character(3)
+x <- logical(3)
+as.numeric(x)                             # Coerced to numeric. 
+
+
+# Programming----------------------------------------------------------------------------------
+
+# Conditional Execution
+'Multi-line Syntax:       Seems a little weird and verbose, but youll need to follow the structure below. 
+                          the condition must be follow if and be wrapped in (). 
+                          then the function to be executed is wrapped in {}. 
+                          There is also an else statement, the function for whcih also needs to be wrapped in {}
+                          Line spacing:  appears that it must e followed directly. '
+x <- 3
+
+if (x>2) {
+  print('x is greater than 2')
+} else {
+  print('x is less than 2')
+}
+
+'Single line conditional statement syntax'
+if (x>2) print('shit') else print('crap')
+
+'Adding conditions:  
+  &, |,                   Apply to a vector element wise
+  &&, ||                  Apply to the first element of a vector
+'
+if (x>2 & x>1) print('shit')
+
+
+# Loops & break statements-------------------------------------------------
+rm(list = ls())
+
+# Iterate over an iterable object
+x <- runif(10, min = 1, max = 20)
+for (i in x) {print(i)}
+
+# Iterate over an object that you create within the for loop
+for (i in 1:10) {x <- i; print(x)}
+
+# Functions---------------------------------------------------------------
+'Syntax
+                         function.name <- function(arguments){
+                              body and return values
+                         }
+'
+
+dumb <- function(){1}
+dumb()                    # dumb() takes no arguments and returns 1. 
+
+dumber <- function(x){x+1}
+dumber(1)                 # dumber() take one input 'x' and returns x+1. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
